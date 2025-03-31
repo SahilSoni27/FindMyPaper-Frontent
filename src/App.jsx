879f9f43a82d0b0db7a995455cf959940f6d87af
Neel <neelganatra25@gmail.com>
2025-03-31 09:49:32 +0530

    fix:/ some ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Sidebar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/previouspaper" element={<PreviousPaper />} />
           <Route path="/notes" element={<Notes />} />
           <Route path="/contactus" element={<ContactUs />} />
