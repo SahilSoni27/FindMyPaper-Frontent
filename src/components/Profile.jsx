0b0911ca4d3064694b51125accd54ee14b6a35c1
SAHIL SONI <133500990+SahilSoni27@users.noreply.github.com>
2025-04-10 13:06:00 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Box, Typography, Avatar, Container, CircularProgress } from '@mui/material';
+
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user, isLoading } = useAuth0();
 
-export default Profile
+  if (isLoading) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        mt={6}
+        p={4}
+        boxShadow={3}
+        borderRadius={4}
+        bgcolor="white"
+      >
+        {/* Avatar Image */}
+        <Avatar
+          src={user.picture}
+          alt={user.name}
+          sx={{
+            width: 120,
+            height: 120,
+            mb: 2,
+          }}
+        />
+
+        {/* User Info */}
+        <Typography variant="h5" fontWeight="bold" gutterBottom>
+          Admin Profile
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary">
+          {user.email}
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Profile;
